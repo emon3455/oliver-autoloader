30557ea894093d0b03ddf9ff9abc1da5251c002e
Emon <emon.mhk69@gmail.com>
2025-12-31 01:19:55 +0600

    unnececary things removed

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-// Jest setup file to set environment variables before tests run
-process.env.ENVIRONMENT = 'local';
-process.env.LOGGING_ENABLED = 'true';
-process.env.LOGGING_ENABLE_CONSOLE_LOGS = 'false'; // Disable console logs during tests for cleaner output
-process.env.LOG_DEBUG_LEVEL = 'debug';
-process.env.LOG_LOCAL_ROOT = './logs';
